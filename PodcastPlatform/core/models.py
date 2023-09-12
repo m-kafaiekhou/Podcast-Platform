a50592cccb634f7f7a03d92ce08f4adfad4fd215
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-09-12 05:06:34 +0330

    chore: Base model abstract class

--- PodcastPlatform/core/models.py
+++ PodcastPlatform/core/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from .managers import BaseManager
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    objects = BaseManager()
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_('Created at'))
+
+    deleted_at = models.DateTimeField(
+        null=True,
+        blank=True,
+        editable=False,
+        verbose_name=_('Deleted datetime'),
+        help_text=_('This is deleted datetime')
+    )
+
+    restored_at = models.DateTimeField(
+        null=True,
+        blank=True,
+        editable=False,
+        verbose_name=_('Restored Datetime'),
+        help_text=_('This is Restored Datetime')
+    )
+
+    is_deleted = models.BooleanField(
+        default=False,
+        editable=False,
+        db_index=True,  # creates an index on the col to improve query speed
+        verbose_name=_('Deleted status'),
+        help_text=_('This is deleted status')
+    )
+
+    def logical_delete(self):
+        self.deleted_at = timezone.now()
+        self.is_deleted = True
+        self.save()
+
+    def restore(self):
+        self.restored_at = timezone.now()
+        self.is_deleted = False
+        self.save()
