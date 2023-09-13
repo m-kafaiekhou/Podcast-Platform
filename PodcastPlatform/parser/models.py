60def10d6c78a5e83020771f49c838bdad7dcc4e
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-09-13 22:28:08 +0330

    chore: podcast rss path

--- PodcastPlatform/parser/models.py
+++ PodcastPlatform/parser/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from core.models import BaseModel
 
-# Create your models here.
+
+class PodcastFormat(models.Model):
+    title = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+
+    summary = models.CharField(max_length=100, null=True, blank=True)
+    image = models.CharField(max_length=100, null=True, blank=True)
+    host = models.CharField(max_length=100, null=True, blank=True)
+    keywords = models.CharField(max_length=100, null=True, blank=True)
+    explicit = models.CharField(max_length=100, null=True, blank=True)
+    copyright = models.CharField(max_length=100, null=True, blank=True)
+    language = models.CharField(max_length=100, null=True, blank=True)
+    link = models.CharField(max_length=100, null=True, blank=True)
+
+
+class PodcastEpisodeFormat(models.Model):
+    audio_file_path = models.CharField(max_length=100)
+    duration_path = models.CharField(max_length=100)
+    title_path = models.CharField(max_length=100)
+    duration = models.CharField(max_length=100)
+    audio_file = models.CharField(max_length=100)
+    publish_date = models.CharField(max_length=100)
+
+    explicit = models.CharField(max_length=100)
+    summary = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    guests = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
