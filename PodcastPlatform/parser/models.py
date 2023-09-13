acea198dc2599ca8f6b8d55d0a61374a2648419a
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-09-14 03:11:33 +0330

    chore: modify parser models

--- PodcastPlatform/parser/models.py
+++ PodcastPlatform/parser/models.py
@@ -2,32 +2,40 @@ from django.db import models
 from core.models import BaseModel
 
 
-class PodcastFormat(models.Model):
-    title = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    owner = models.CharField(max_length=100)
-
-    summary = models.CharField(max_length=100, null=True, blank=True)
-    image = models.CharField(max_length=100, null=True, blank=True)
-    host = models.CharField(max_length=100, null=True, blank=True)
-    keywords = models.CharField(max_length=100, null=True, blank=True)
-    explicit = models.CharField(max_length=100, null=True, blank=True)
+class PodcastFormat(BaseModel):
+    title = models.CharField(max_length=100, null=True, blank=True)
+    description = models.CharField(max_length=100, null=True, blank=True)
     copyright = models.CharField(max_length=100, null=True, blank=True)
-    language = models.CharField(max_length=100, null=True, blank=True)
+    generator = models.CharField(max_length=100, null=True, blank=True)
     link = models.CharField(max_length=100, null=True, blank=True)
+    owner_name = models.CharField(max_length=100, null=True, blank=True)
+    owner_email = models.CharField(max_length=100, null=True, blank=True)
+    author = models.CharField(max_length=100, null=True, blank=True)
+    summary = models.CharField(max_length=100, null=True, blank=True)
+    language = models.CharField(max_length=100, null=True, blank=True)
+    explicit = models.CharField(max_length=100, null=True, blank=True)
+    category = models.CharField(max_length=100, null=True, blank=True)
+    keywords = models.CharField(max_length=100, null=True, blank=True)
+    type = models.CharField(max_length=100, null=True, blank=True)
+    icon_image_url = models.CharField(max_length=100, null=True, blank=True)
+    image_url = models.CharField(max_length=100, null=True, blank=True)
+    image_link = models.CharField(max_length=100, null=True, blank=True)
+    image_title = models.CharField(max_length=100, null=True, blank=True)
 
 
-class PodcastEpisodeFormat(models.Model):
-    audio_file_path = models.CharField(max_length=100)
-    duration_path = models.CharField(max_length=100)
-    title_path = models.CharField(max_length=100)
-    duration = models.CharField(max_length=100)
-    audio_file = models.CharField(max_length=100)
-    publish_date = models.CharField(max_length=100)
-
-    explicit = models.CharField(max_length=100)
-    summary = models.CharField(max_length=100)
+class PodcastEpisodeFormat(BaseModel):
+    title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    guests = models.CharField(max_length=100)
+    episode_type = models.CharField(max_length=100)
+    episode_num = models.CharField(max_length=100)
+    summary = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
+    guid = models.CharField(max_length=100)
+    publish_date = models.CharField(max_length=100)
+    explicit = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=100)
     keywords = models.CharField(max_length=100)
-    image = models.CharField(max_length=100)
+    duration = models.CharField(max_length=100)
+    enclosure_url = models.CharField(max_length=100)
+    enclosure_type = models.CharField(max_length=100)
+    enclosure_length = models.CharField(max_length=100)
