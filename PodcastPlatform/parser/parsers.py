ef0247890924821921628b3a54e19169ba9f2b0d
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-09-14 05:50:36 +0330

    chore: init rss parser class

---
+++ PodcastPlatform/parser/parsers.py
@@ -0,0 +1,22 @@
+import requests
+from xml.etree import ElementTree as ET
+
+
+response = requests.get('https://rss.art19.com/apology-line')
+print(type(response.content))
+
+root = ET.fromstring(response.content)
+
+print(root.find('channel/item').find('title').text)
+print(root.find('channel/title').text)
+print(root.find('channel/{http://www.itunes.com/dtds/podcast-1.0.dtd}category').get('text'))
+
+
+
+class RSSParser:
+    def __init__(self, podcastModel: object, episodeModel: object):
+        self.podcastModel = podcastModel
+        self.episodeModel = episodeModel
+
+    
+    
