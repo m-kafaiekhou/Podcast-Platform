dd0a14842e23523d0bfc6cb99e842b3d2f93406b
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-09-15 07:35:48 +0330

    chore: blueprint of the jwt functions

---
+++ PodcastPlatform/accounts/utils.py
@@ -0,0 +1,48 @@
+
+
+def create_access_token(user):
+    """
+    Creates an access token for the user
+
+    args:
+        user => user that sent the request
+    return:
+        access token
+    """
+    pass
+
+
+def decrypt_access_token(token):
+    """
+    decrypts access token
+
+    args:
+        token => encrypted token that was given by the user
+    return:
+        decrypted access token
+    """
+    pass
+
+
+def create_refresh_token(user):
+    """
+    Creates a refresh token for the user
+
+    args:
+        user => user that sent the request
+    return:
+        refresh token
+    """
+    pass
+
+
+def decrypt_refresh_token(token):
+    """
+    decrypts refresh token
+
+    args:
+        token => encrypted token that was given by the user
+    return:
+        decrypted refresh token
+    """
+    pass
