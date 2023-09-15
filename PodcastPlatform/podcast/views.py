416b2dc700893bc64d9ecf5c2d5d2739049ab029
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-09-15 04:39:56 +0330

    chore: add podcast and episode serializers

--- PodcastPlatform/podcast/views.py
+++ PodcastPlatform/podcast/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, views
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Podcast, PodcastEpisode
+from .serializers import PodcastSerializer, PodcastEpisodeSerializer
+
+
+class PodcastListCreateView(generics.ListAPIView):
+    serializer_class = PodcastSerializer
+    queryset = Podcast.objects.get_active_list()
+
+
+class PodcastEpisodeListCreateView(views.APIView):
+
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        podcast = Podcast.objects.get(pk=pk)
+        episode_queryset = PodcastEpisode.objects.filter(podcast__id=pk, is_deleted=False)
+
+        podcast_serializer = PodcastSerializer(podcast)
+        episode_serializer = PodcastEpisodeSerializer(episode_queryset, many=True)
+
+        data = {
+            'podcast': podcast_serializer.data,
+            'episodes': episode_serializer.data
+            }
+        
+        return Response(data=data)
