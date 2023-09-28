8e4dcb12394db6e32f1d3b927cbb008d0555a8ce
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-09-28 16:49:24 +0330

    fix: authenticate use id to get user and jwt method encodes token

--- PodcastPlatform/accounts/authentications.py
+++ PodcastPlatform/accounts/authentications.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework import authentication
 from rest_framework.exceptions import AuthenticationFailed, ParseError
-from .utils import check_cache
+from .utils import check_cache, encode_payload, decode_token
 
 User = get_user_model()
 
@@ -14,12 +14,12 @@ class JWTAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         jwt_token = request.META.get('HTTP_AUTHORIZATION')
         if jwt_token is None:
-            return None
+            raise AuthenticationFailed("Token not available")
 
         jwt_token = JWTAuthentication.get_the_token_from_header(jwt_token)
 
         try:
-            payload = jwt.decode(jwt_token, settings.SECRET_KEY, algorithms=['HS256'])
+            payload = decode_token(jwt_token)
         except jwt.exceptions.InvalidSignatureError:
             raise AuthenticationFailed('Invalid signature')
         except Exception as e:
@@ -29,11 +29,11 @@ class JWTAuthentication(authentication.BaseAuthentication):
         if not check_cache(payload.get('jti')):
             raise AuthenticationFailed('user not in whitelist')
 
-        phone_number = payload.get('user_identifier')
-        if phone_number is None:
+        user_id = payload.get('user_identifier')
+        if user_id is None:
             raise AuthenticationFailed('User identifier not found in JWT')
 
-        user = User.objects.filter(phone_number=phone_number).first()
+        user = User.objects.filter(id=user_id).first()
         if user is None:
             raise AuthenticationFailed('User not found')
 
@@ -58,7 +58,7 @@ class JWTAuthentication(authentication.BaseAuthentication):
             'jti': jti,
         }
 
-        access_jwt_token = jwt.encode(access_payload, settings.SECRET_KEY, algorithm='HS256')
+        access_jwt_token = encode_payload(access_payload)
 
         refresh_payload = {
             'user_identifier': user_id,
@@ -67,7 +67,7 @@ class JWTAuthentication(authentication.BaseAuthentication):
             'jti': jti,
         }
 
-        refresh_jwt_token = jwt.encode(refresh_payload, settings.SECRET_KEY, algorithm='HS256')
+        refresh_jwt_token = encode_payload(refresh_payload)
 
         return access_jwt_token, refresh_jwt_token
 
