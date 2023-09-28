e643ece8198f0c858b619c6602a8eb1ee0910872
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-09-28 20:23:52 +0330

    chore: add logger for running and completed tasks

--- PodcastPlatform/parser/tasks.py
+++ PodcastPlatform/parser/tasks.py
@@ -13,9 +13,16 @@ logger = logging.getLogger('celery-log')
 @shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={'max_retries': 5}, retry_jitter=True, task_time_limit=600, retry_backoff=2)
 def parse_feeds_to_db(self, podcast_pk):
     try:
+        retry_count = self.request.retries
+        message = f"running task parse_feeds_to_db for the {retry_count} time"
+        logger.info(message)
+
         podcast_obj = Podcast.objects.get(pk=podcast_pk)
         parser = PodcastRSSParser(podcast_obj, PodcastEpisode)
         parser.fill_db()
+
+        message = f"ran task parse_feeds_to_db for the {retry_count} time"
+        logger.info(message)
     except Exception as e:
         retry_count = self.request.retries
         message = f"task parse_feeds_to_db failed for the {retry_count} time"
@@ -30,8 +37,14 @@ def parse_feeds_to_db(self, podcast_pk):
 @shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={'max_retries': 5}, retry_jitter=True, task_time_limit=600, retry_backoff=2)
 def podcast_parse_task(self):
     try:
+        retry_count = self.request.retries
+        message = f"running task podcast_parse_task for the {retry_count} time"
+        logger.info(message)
+
         podcasts = Podcast.objects.all()
 
+        message = f"ran task podcast_parse_task for the {retry_count} time"
+        logger.info(message)
     except Exception as e:
         retry_count = self.request.retries
         message = f"task podcast_parse_task failed for the {retry_count} time"
