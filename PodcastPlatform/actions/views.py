87ed1ffb4306ef5dca4f0caee8410f5c23402b02
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-10-05 09:57:06 +0330

    chore: comment view

--- PodcastPlatform/actions/views.py
+++ PodcastPlatform/actions/views.py
@@ -14,4 +14,18 @@ class SubscriptionView(InteractionMixin, APIView):
     model = Podcast
 
 
+class CommentView(InteractionMixin, APIView):
+    action_model = Comment
+    model = Podcast
+
+    serializer_class = CommentSerializer
+
+
+    def post(self, request, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        kwargs['content'] = serializer.validated_data['content']
+
+        return super().post(request, **kwargs)
+
 
