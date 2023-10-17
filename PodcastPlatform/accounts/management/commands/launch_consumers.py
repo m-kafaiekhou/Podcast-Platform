0c832fe89ebc123ee1a0dc71d162b374e42915e1
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-10-18 02:16:28 +0330

    fix: podcast update notification consumer

--- PodcastPlatform/accounts/management/commands/launch_consumers.py
+++ PodcastPlatform/accounts/management/commands/launch_consumers.py
@@ -1,10 +1,16 @@
 from django.core.management.base import BaseCommand
-from accounts.consumer import auth_notif_consumer
+from multiprocessing import Process
+
+from accounts.consumer import auth_notif_consumer, podcast_update_consumer
 
 
 class Command(BaseCommand):
-    help = 'Launches Consumer for login message : RabbitMQ'
+    help = 'Launches Consumers : RabbitMQ'
 
     def handle(self, *args, **options):
-        auth_notif_consumer()
+        p1 = Process(target=auth_notif_consumer)
+        p2 = Process(target=podcast_update_consumer)
+        
+        p1.start()
+        p2.start()
         
