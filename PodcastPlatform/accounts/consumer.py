f5a89ca7b0a4e6ba72423d1facc7f977aae594b4
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-10-18 02:17:08 +0330

    chore: add podcast notif consumer

--- PodcastPlatform/accounts/consumer.py
+++ PodcastPlatform/accounts/consumer.py
@@ -5,6 +5,7 @@ from sys import path
 from os import environ
 
 from .models import Notification
+from podcast.models import Podcast
 
 
 path.append('/home/Desktop/maktab/Podcast-Platform/PodcastPlatform/config/settings.py')
@@ -31,16 +32,47 @@ def callback(ch, method, properties, body):
         print("quote deleted")
 
 
-def auth_notif_consumer():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', heartbeat=600))
+def callback_podcast_update(ch, method, properties, body):
+    print("Received in likes...*****#*#*#**##****")
+    data = json.loads(body)
+    print(data, "*********###*#*#*#*#**##**")
+    podcast = Podcast.objects.get(id=data['podcast'])
 
-    print("be channel**************************************************")
+    users = data['users']
+
+    notif_lst = []
+
+    for user in users:
+        notif = Notification(title='o', user_id=user, message=f'podcast {podcast.title} added new episodes')
+        notif_lst.append(notif)
+
+    ns = Notification.objects.bulk_create(notif_lst)
+    print(ns, "ghghghghghghghghgjfjfjfj")
+    print("update notif created successfully")
+    
+
+
+def auth_notif_consumer():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
     channel = connection.channel()
-    print("af channel*************************************************")
 
     channel.queue_declare(queue='auth-notification')
-
-            
+      
     channel.basic_consume(queue='auth-notification', on_message_callback=callback, auto_ack=True)
     print("Started Consuming...")
     channel.start_consuming()
+
+
+def podcast_update_consumer():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+
+    print("be channel**********************************podcast_update**********")
+    channel = connection.channel()
+    print("af channel*************************************************")
+
+    channel.queue_declare(queue='podcast_update')
+            
+    channel.basic_consume(queue='podcast_update', on_message_callback=callback_podcast_update, auto_ack=True)
+    print("Started Consuming...")
+    channel.start_consuming()
+
