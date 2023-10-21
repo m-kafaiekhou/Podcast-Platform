94a23453ded2ce4113232cccf0f3770d472b5479
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-10-21 23:55:27 +0330

    refactor: clean up

--- PodcastPlatform/accounts/producer.py
+++ PodcastPlatform/accounts/producer.py
@@ -3,11 +3,9 @@ import pika
 
 
 def publish(method, body, queue):
-    print("publisher ******** doing publishing ##*#####")
     connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', heartbeat=600))
     channel = connection.channel()
     channel.queue_declare(queue=queue)
     properties = pika.BasicProperties(method, delivery_mode=2)
     channel.basic_publish(exchange='', routing_key=queue, body=json.dumps(body), properties=properties)
-    print("publisher ******** done publishing ##*#####")
     connection.close()
