bf94c9955810570e0ed11fdfa7a23e17662f2668
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-10-24 16:07:39 +0330

    chore: recommendation view

--- PodcastPlatform/podcast/views.py
+++ PodcastPlatform/podcast/views.py
@@ -1,10 +1,12 @@
-from rest_framework import generics, views
-from rest_framework.response import Response
+from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination
-from rest_framework import serializers
+from rest_framework.permissions import IsAuthenticated
+from django.db.models.aggregates import Count
+
 
 from .models import Podcast, PodcastEpisode
 from .serializers import PodcastSerializer, PodcastEpisodeSerializer
+from accounts.authentications import JWTAuthentication
 
 
 class CustomPagination(PageNumberPagination):
@@ -33,3 +35,41 @@ class PodcastEpisodeDetailView(generics.RetrieveAPIView):
     queryset = PodcastEpisode.objects.get_active_list()
     serializer_class = PodcastEpisodeSerializer
     lookup_field = 'pk'
+
+
+class GetRecommendationsView(generics.ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = PodcastSerializer
+    pagination_class = CustomPagination
+
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_authenticated:
+            self.queryset = self.get_podcast_based_on_likes(user)
+        else:
+            self.queryset = Podcast.objects.annotate(num_likes=Count('podcastepisode__like')).order_by('-num_likes')
+        
+        return super().get_queryset()
+
+    def get_podcast_based_on_likes(self, user):
+        podcasts = Podcast.objects.all()
+        podcast_episodes = PodcastEpisode.objects.select_related("podcast__category").prefetch_related('like').filter(like__user=user)
+        
+        rate_dict = {}
+
+        for ep in podcast_episodes:
+            category = ep.podcast.category
+            rate_dict.setdefault(category, 0)
+            rate_dict[category] += 1
+
+        sorted_rate = [k for k,_ in sorted(rate_dict.items(), key=lambda item:item[1], reverse=True)]
+
+        categories = sorted_rate[:4] if len(sorted_rate) > 4 else sorted_rate
+
+        qs = podcasts.filter(category__in=categories)
+
+        return qs
+    
