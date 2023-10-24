9e5053837dc514e7c324b064f12fc0b0f1318587
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-10-25 02:53:45 +0330

    chore: custom exception for exp token

---
+++ PodcastPlatform/accounts/exceptions.py
@@ -0,0 +1,12 @@
+from rest_framework.exceptions import APIException
+from rest_framework import status
+from django.utils.translation import gettext_lazy as _
+
+class TokenExpired(APIException):
+    def __init__(self):
+        detail = "Token expired"
+        super().__init__(detail, 470)
+
+    status_code = 470
+    default_detail = _('Token expired')
+    default_code = 'token_expired'
