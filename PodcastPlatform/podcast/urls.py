dd1b519c710dd4911f313d6c59cf0f7f9b15ee2b
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-10-24 16:07:18 +0330

    refactor: add base url to podcasts urls

--- PodcastPlatform/podcast/urls.py
+++ PodcastPlatform/podcast/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from .views import PodcastListView, PodcastEpisodeListView, PodcastEpisodeDetailView
+from .views import (
+        PodcastListView,
+        PodcastEpisodeListView,
+        PodcastEpisodeDetailView,
+        GetRecommendationsView)
 
 app_name = 'podcast'
 
 urlpatterns =[
-    path("podcasts/", PodcastListView.as_view(), name="podcast-list"),
-    path("podcasts/episodes/<int:pk>/", PodcastEpisodeListView.as_view(), name="podcast-episode-list"),
-    path("podcasts/episode/detail/<int:pk>/", PodcastEpisodeDetailView.as_view(), name="podcast-episode-detail"),
+    path("", PodcastListView.as_view(), name="podcast-list"),
+    path("episodes/<int:pk>/", PodcastEpisodeListView.as_view(), name="podcast-episode-list"),
+    path("episode/detail/<int:pk>/", PodcastEpisodeDetailView.as_view(), name="podcast-episode-detail"),
+    path("recommendations/", GetRecommendationsView.as_view(), name="recommendation"),
+
 ]
     
